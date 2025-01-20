694d4670a17fda45a6516a94d2a0891b67a565c6
John-Rashta <john_rashta@outlook.com>
2025-01-20 23:28:56 +0000

    feat: basic template

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,14 @@
+// App.test.jsx
+
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+
+describe("App component", () => {
+  it("renders magnificent monkeys", () => {
+    // since screen does not have the container property, we'll destructure render to obtain a container for this test
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+});
